61b5eff893eeb6af83080804128293d218c3bca4
Felician Nemeth <nemethf@tmit.bme.hu>
2012-07-25 06:28:22 -0700

    sigcomm2012, first version

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((nil . ((compile-command . "cd ~/of11softswitch && make -k ")))
+ (c-mode . ((c-basic-offset . 4)
+	    (ffap-c-path "/usr/include"
+			 "/usr/local/include"
+			 "~/of11softswitch/")
+	    (cc-search-directories "."
+				   "/usr/include"
+				   "/usr/local/include/*"
+				   "~/of11softswitch/"))))
