805326af457603a8f7d8c90eb3ff5aca717642f8
Felician Nemeth <nemethf@tmit.bme.hu>
2012-08-06 07:08:11 -0700

    remove .dir-locals.el

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-((nil . ((compile-command . "cd ~/of11softswitch && make -k ")))
- (c-mode . ((c-basic-offset . 4)
-	    (ffap-c-path "/usr/include"
-			 "/usr/local/include"
-			 "~/of11softswitch/")
-	    (cc-search-directories "."
-				   "/usr/include"
-				   "/usr/local/include/*"
-				   "~/of11softswitch/"))))
